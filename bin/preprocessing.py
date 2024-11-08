c73a237b500066553bad57d4759d1d4368d1286a
Mamie Wang <wangm10@mskcc.org>
2024-11-08 11:15:09 -0500

    Incorporated regression variables to preprocessing

--- bin/preprocessing.py
+++ bin/preprocessing.py
@@ -1,6 +1,5 @@
 import os
 import argparse
-from scipy.io import mmwrite
 import pandas as pd
 from CellAlignDTW.pp import create_cellrank_probability_df
 
@@ -42,15 +41,35 @@ RU1646,NSCLC,SCLC-N_3
     h5ad_files = [os.path.join(args.path, x + ".no_cc.hvg_2000.090124.h5ad") for x in samples]
     combined_adata, df = create_cellrank_probability_df(h5ad_files, 'cell_type_final2', samples, 
                                         result_dict, clusters)
+    
     print(f"Pseudobulking for cell types: {clusters}")
     # Create a DataFrame from the counts layer
-    counts_df = pd.DataFrame(combined_adata.layers['counts'], 
-                                                index=combined_adata.obs_names, 
-                                                columns=combined_adata.var_names)
-    counts_df['sample'] = df['sample'].values
-    counts_df['cell_type'] = df['cell_type'].values
-    summed_counts = counts_df.groupby(['sample', 'cell_type']).sum().reset_index()
-    print(summed_counts.shape)
+    counts_df = pd.DataFrame(combined_adata.layers['counts'], index=combined_adata.obs_names, columns=combined_adata.var_names)
+    counts_df_metadata = pd.DataFrame({
+        'patient': df['sample'].values,
+        'cell_type': df['cell_type'].values,
+        'chemo': combined_adata.obs['chemo'].values,
+        'IO': combined_adata.obs['IO'].values,
+        'TKI': combined_adata.obs['TKI'].values,
+        'tissue': combined_adata.obs['tissue'].values,
+        'V2V3': combined_adata.obs['ten_x_version'].values,
+        'sample': combined_adata.obs_names.str.split('_').str[:2].str.join('_')
+    })
+    counts_df['patient'] = counts_df_metadata['patient'].values
+    counts_df['sample'] = counts_df_metadata['sample'].values
+    summed_counts = counts_df.groupby(['patient', 'sample', 'cell_type']).sum().reset_index()
+    
+    metadata_age = pd.read_table('/data1/chanj3/Xenium.lung.NE_plasticity.010124/ref/metadata.072523/Rudin Lab NE transformation 040723_AG_CF.JC.subset_to_transformed.txt')
+    metadata_age = pd.DataFrame({'patient': metadata_age['Rudin Patient ID'].apply(lambda x: "RU" + str(x)),
+                                'age': metadata_age['Age at Collection'],
+                                'sex': metadata_age['Sex'],
+                                'race': metadata_age['Race'],
+                                'smoking': metadata_age['Smoking'],
+                                'EGFR_status': metadata_age['EGFR']})
+    metadata_age = metadata_age.groupby('patient').first().reset_index()
+    counts_df_metadata = counts_df_metadata.merge(metadata_age, on=['patient'], how='left')
+    counts_df_metadata = counts_df_metadata.drop_duplicates().reset_index(drop=True)
+    summed_counts = summed_counts.merge(counts_df_metadata, on=['patient', 'sample'], how='left')
 
     print("Saving results...")
     summed_counts.to_csv(os.path.join(args.outpath, f'counts.{args.clusters}.csv'))
